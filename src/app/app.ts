7175ce02511a37b54a58bcb934d53a78012ef709
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-08-11 17:50:49 +0530

    routing primary done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
+// import { Home } from './home/home';
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header],
+  // imports: [RouterOutlet, Home, Header],
+  imports: [RouterOutlet,Header],
   template: `
     <app-header></app-header>
     <main>
-      <app-home/>
+      <!-- <app-home/> -->
+       <router-outlet />
     </main>
-    <router-outlet />
   `,
   styles: [
     `
