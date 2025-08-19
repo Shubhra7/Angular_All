5e18afac06f6f607670400b1de1c28883a36bacc
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-08-19 17:29:16 +0530

    angular pipeline and searching features making done

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Todo } from '../../model/todo.type';
 import { HighlightCompletedTodos } from '../../directives/highlight-completed-todos';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [ HighlightCompletedTodos],
+  imports: [ HighlightCompletedTodos, UpperCasePipe],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss'
 })
